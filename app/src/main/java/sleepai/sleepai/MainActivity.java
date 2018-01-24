cc91d08f0f630150494919c562726da90398cdd1
Arijit Ghosh Chowdhury <arijit10@gmail.com>
2018-01-25 01:26:55 +0530

    Initial commit

---
+++ app/src/main/java/sleepai/sleepai/MainActivity.java
@@ -0,0 +1,25 @@
+package sleepai.sleepai;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("http://sleep-ai.azurewebsites.net");
+
+        getSupportActionBar().hide();
+    }
+}
